0cb3349502fb8e724ae3a519ce087a7affa27d5e
astonelv <158239756+astonelv@users.noreply.github.com>
2024-05-20 18:07:55 -0700

    a

--- views/default.jsx
+++ views/default.jsx
@@ -5,8 +5,8 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                <link href= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
-                <link rel="stylesheet" href="public/css/style.css"/>
+                <link rel="stylesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
+                <link rel="stylesheet" href="static\css\style.css"/>
             </head>
             <body>
                 {html.children}
