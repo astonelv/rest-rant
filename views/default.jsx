b516e11207082aedecf7a41fef646dacf1ce99e5
astonelv <158239756+astonelv@users.noreply.github.com>
2024-05-29 17:49:56 -0700

    fixed edit routes

--- views/default.jsx
+++ views/default.jsx
@@ -6,7 +6,7 @@ function Def (html) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
-                <link rel="stylesheet" href= "public/index.css"/>
+                <link rel="stylesheet" href= "/css/index.css"/>
             </head>
             <body>
                 <nav>
