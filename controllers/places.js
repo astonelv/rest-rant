0deab1588ea0dacbd1ade7b4206b4bbd4f6c764e
astonelv <158239756+astonelv@users.noreply.github.com>
2024-06-19 22:37:38 -0700

    finish

--- controllers/places.js
+++ controllers/places.js
@@ -1,92 +1,140 @@
 const router = require('express').Router()
-const places = require("../models/places")
+const db = require('../models')
 
-router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-router.get('/', (req, res) => {
-  res.render('places/index', { places })
-})
-
+// NEW
 router.get('/new', (req, res) => {
   res.render('places/new')
+})
+
+// INDEX
+router.get('/', (req, res) => {
+  db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error404')
+    })
+})
+
+// PLACE
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect('/places')
+    })
+    .catch(err => {
+      if (err && err.name == 'ValidationError') {
+        let message = 'Validation Error: '
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+        }
+        console.log('Validation error message', message)
+        res.render('places/new', { message })
+      }
+      else {
+        res.render('error404')
+      }
+    })
+})
+
+// SHOW
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+    .populate('comments')
+    .then(place => {
+      console.log(place.comments)
+      res.render('places/show', { place })
+    })
+    .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+    })
+})
+
+// CREATE COMMENT
+router.post("/:id/comment", (req, res) => {
+  console.log(req.body);
+
+  // Ensure that the stars field is correctly parsed as a number
+  req.body.stars = parseFloat(req.body.stars);
+  req.body.rant = req.body.rant ? true : false;
+
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place
+            .save()
+            .then(() => {
+              res.redirect(`/places/${req.params.id}`);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.render("error404");
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
 });
 
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  } else if (!places[id]) {
-    res.render('error404')
-  } else {
-    console.log('id', id)
-    res.render('places/show', { place: places[id], id })
-  }
+// UPDATE
+router.put('/:id', (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+    })
+    .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+    })
 })
 
+// DELETE
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+  db.Place.findByIdAndDelete(req.params.id)
+    .then(place => {
+      res.redirect('/places')
+    })
+    .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+    })
 })
 
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] , id})
-  }
+// EDIT 
+router.get("/:id/edit", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/edit", { place });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
+
+// Rant
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
 })
 
-// Put Route
-  router.put('/:id', (req, res) => {
-    let i = Number(req.params.id)
-    if (isNaN(i)) {
-        res.render('error404')
-    }
-    else if (!places[i]) {
-        res.render('error404')
-    }
-    else {
-        // Dig into req.body and make sure data is valid
-        if (!req.body.pic) {
-            req.body.pic = 'http://placekitten.com/400/400'
-        }
-        if (!req.body.city) {
-            req.body.city = 'Anytown'
-        }
-        if (!req.body.state) {
-            req.body.state = 'USA'
-        }
-        // Save the new data into places[i]
-        places[i] = req.body
-        res.redirect(`/places/${i}`)
-    }
-  })  
+// DELETE Stub
+router.delete('/:id/rant/:rantId', (req, res) => {
+  res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
---
+++ models/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB:', process.env.MONGO_URI);
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+module.exports.Place = require('./places');
+module.exports.Comment = require('./comment')
