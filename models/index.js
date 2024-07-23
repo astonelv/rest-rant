400645d7623188bf2a354e63acae935ba6c13038
astonelv <158239756+astonelv@users.noreply.github.com>
2024-07-23 03:23:26 -0700

    rest-rant 8

--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB:', process.env.MONGO_URI);
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
-module.exports.Place = require('./places')
+module.exports.Place = require('./places');
