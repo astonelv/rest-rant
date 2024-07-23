ce32fdc84ffcf42efaba4fe154d1df6142672c54
astonelv <158239756+astonelv@users.noreply.github.com>
2024-07-23 03:46:16 -0700

    rest-rant 9

--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB:', process.env.MONGO_URI);
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
 
-module.exports.Place = require('./places');
+module.exports.Place = require('./places')
+
