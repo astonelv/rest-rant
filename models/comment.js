bb79e661880bc0b5153d5ce2d047c039f5b850af
astonelv <158239756+astonelv@users.noreply.github.com>
2024-07-23 00:35:20 -0700

    k

--- models/comment.js
+++
@@ -1,6 +0,0 @@
-let commentSchema = new mongoose.Schema({
-    stars: { type: Number, required: true },
-    content: { type: String, default: '' }
-})
-  
-module.exports = mongoose.model('Comment', commentSchema)
